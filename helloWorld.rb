f35c1d1de5658282c8a05da8abc052b3651f76ad
eureka1 <lakayiiii@gmail.com>
2014-10-03 19:46:40 -0400

    s1-8 finished

--- helloWorld.rb
+++ helloWorld.rb
@@ -477,7 +477,6 @@ class Solution
       end
     end
     country_list.sort!.reverse!
-    country_list.each{ |x| puts x}
     result_list = []
     i = 0
     while i < topNumber
@@ -495,12 +494,12 @@ s = Solution.new
 s.get_all_countries
 
 #puts country_lists.keys
-#s.s1_search_natural_hazards("South America", "earthquake")
-#s.s2_search_lowest_elevation_point("Europe")
-#s.s3_search_hemisphere("southerneast")
-#s.s4_search_party_number("Asia", 10)
-#s.s5_search_top_electricity_consumption(5)
-#s.s6_search_domain_region(true, 80)
-#s.s6_search_domain_region(false, 50)
-#s.s7_search_landlocked()
+s.s1_search_natural_hazards("South America", "earthquake")
+s.s2_search_lowest_elevation_point("Europe")
+s.s3_search_hemisphere("southerneast")
+s.s4_search_party_number("Asia", 10)
+s.s5_search_top_electricity_consumption(5)
+s.s6_search_domain_region(true, 80)
+s.s6_search_domain_region(false, 50)
+s.s7_search_landlocked()
 s.s8_search_top_coastline(10)
